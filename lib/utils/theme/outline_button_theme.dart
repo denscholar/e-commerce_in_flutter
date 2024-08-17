3052b41aab2d668aa0a90d658030cd98269aff7a
Dennis Akagha <d.akagha20@gmail.com>
2024-08-17 03:25:20 +0100

    add theme setup and other ui components

---
+++ lib/utils/theme/outline_button_theme.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class TOutlineButtonTheme {
+  TOutlineButtonTheme._();
+
+// white screen
+  static final lightOutlineButtonTheme = OutlinedButtonThemeData(
+    style: OutlinedButton.styleFrom(
+      elevation: 0,
+      foregroundColor: Colors.black,
+      side: const BorderSide(color: Colors.blue),
+      textStyle: const TextStyle(
+          fontSize: 16.0, color: Colors.black, fontWeight: FontWeight.w600),
+      padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 20.0),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(14)),
+    ),
+  );
+
+// black screen
+  static final darkOutlineButtonTheme = OutlinedButtonThemeData(
+    style: OutlinedButton.styleFrom(
+      elevation: 0,
+      foregroundColor: Colors.white,
+      side: const BorderSide(color: Colors.blueAccent),
+      textStyle: const TextStyle(
+          fontSize: 16.0, color: Colors.black, fontWeight: FontWeight.w600),
+      padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 20.0),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(14)),
+    ),
+  );
+}
